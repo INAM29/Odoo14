07fa118777b24ddc6300db2a88763c3acc58a87e
Inamur Rahiman M <inamrahman29@gmail.com>
2022-12-05 13:17:50 +0530

    first commit

---
+++ inherited_customer/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inherited Customer',
+    'version': '14.0',
+    'summary': 'Inherited Customer module',
+    'sequence': 10,
+    'description': """Inherited Customer module""",
+    'website': 'https://www.odoo.com',
+    'depends': ['base'],
+    'data': [ 'views/inherit_customer_view.xml'
+              ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
